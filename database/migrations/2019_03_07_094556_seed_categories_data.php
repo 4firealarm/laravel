9553108a046af8a0be002b2029f02b6aa5a2a1d5
michshelle <shingyi.sung@gmail.com>
2020-04-10 21:55:14 +0800

    still for categories seed data

--- database/migrations/2019_03_07_094556_seed_categories_data.php
+++ database/migrations/2019_03_07_094556_seed_categories_data.php
@@ -14,18 +14,22 @@ class SeedCategoriesData extends Migration
     {
         $data = [
             [
-                'name' => '分享',
+				'id' => 'daydayup',
+                'name' => '天天向上',
                 'description' => '😭 悲伤可以自行料理；而欢乐的滋味如果要充分体会，你就必须有人分享才行。',
             ],
             [
-                'name' => '教程',
+				'id' => 'climber',
+                'name' => '职场那些事',
                 'description' => '✍️ 在寻求真理的长河中，唯有学习，不断地学习，勤奋地学习，有创造性地学习，才能越重山跨峻岭。',
             ],
             [
-                'name' => '对话',
+				'id' => 'bebetter',
+                'name' => '闲话生活',
                 'description' => '😀 与人交谈一次，往往比多年闭门劳作更能启发心智。思想必定是在与人交往中产生，而在孤独中进行加工和表达。',
             ],
             [
+				'id' => 'announcements',
                 'name' => '公告',
                 'description' => '✅ 我们不应该像蚂蚁，单只收集；也不可像蜘蛛，只从自己肚中抽丝；而应该像蜜蜂，既采集又整理，这样才能酿出香甜蜂蜜来。',
             ],
